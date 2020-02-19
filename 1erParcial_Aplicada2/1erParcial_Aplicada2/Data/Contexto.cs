241a3fbd40ec9e16fb4ee7afdf29be092f45f264
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-02-19 17:52:53 -0400

    Empezando

---
+++ 1erParcial_Aplicada2/1erParcial_Aplicada2/Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1erParcial_Aplicada2.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Database/ParcialDb");
+        }
+    }
+}
