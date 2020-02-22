0fff5b25bc6db961c9afec4c62229485e8f5b162
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-02-22 10:55:44 -0400

    Actualizando la Entidad inscripciones

--- 1erParcial_Aplicada2/1erParcial_Aplicada2/Models/Inscripciones.cs
+++ 1erParcial_Aplicada2/1erParcial_Aplicada2/Models/Inscripciones.cs
@@ -11,8 +11,12 @@ namespace _1erParcial_Aplicada2.Models
         [Key]
         [Required(ErrorMessage = "El campo InscripcionId no puede ser negativo ni estar vacío")]
         public int InscripcionId { get; set; }
-        [Required(ErrorMessage ="El campo Semestre no puede estar vacío")]
+        public DateTime Fecha { get; set; }
+        [Required(ErrorMessage = "El campo Semestre no puede estar vacío")]
         public string Semestre { get; set; }
+        [Required]
+        [Range(minimum: 1, maximum: 1000000000000, ErrorMessage = "El campo EstudianteId tiene que estar registrado ")]
+        public int EstudianteId { get; set; }
         [Required]
         [Range(minimum:1,maximum:10000,ErrorMessage ="Tiene que poner un valor valido en campo Limite")]
         public int Limite { get; set; }
@@ -22,10 +26,17 @@ namespace _1erParcial_Aplicada2.Models
         [Required]
         [Range(minimum: 0, maximum: 10000, ErrorMessage = "Los creditps tomados no pueden superar a los creditos limites")]
         public int Disponibles { get; set; }
+        [Required]
+        [Range(minimum: 0, maximum: 1000000000000, ErrorMessage = "El campo Monto ")]
+        public decimal Monto { get; set; }
+        [Required]
+        [Range(minimum: 0, maximum: 1000000000000, ErrorMessage = "Tiene que poner un valor valido en campo Limite")]
+        public decimal Balance { get; set; }
 
         public Inscripciones()
         {
             InscripcionId = 0;
+            Fecha = DateTime.Now;
             Semestre = string.Empty;
             Limite = 0;
             Tomados = 0;
