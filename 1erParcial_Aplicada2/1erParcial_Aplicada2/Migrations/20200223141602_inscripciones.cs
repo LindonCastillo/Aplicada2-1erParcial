859adfc402f4ea633cfb2926daf88b356ad0a9ca
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-02-23 11:11:45 -0400

    Consultas de los registros y el boton de ver

---
+++ 1erParcial_Aplicada2/1erParcial_Aplicada2/Migrations/20200223141602_inscripciones.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _1erParcial_Aplicada2.Migrations
+{
+    public partial class inscripciones : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Semestre",
+                table: "Inscripciones",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "Inscripciones",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EstudianteId",
+                table: "Inscripciones",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Fecha",
+                table: "Inscripciones",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Monto",
+                table: "Inscripciones",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "Asignaturas",
+                columns: table => new
+                {
+                    AsignaturaId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Codigo = table.Column<string>(nullable: false),
+                    Descripcion = table.Column<string>(nullable: false),
+                    PreRequisito = table.Column<string>(nullable: false),
+                    Creditos = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Asignaturas", x => x.AsignaturaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estudiantes",
+                columns: table => new
+                {
+                    EstudianteId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Matricula = table.Column<string>(maxLength: 10, nullable: false),
+                    Nombres = table.Column<string>(maxLength: 100, nullable: false),
+                    Balance = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiantes", x => x.EstudianteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pagos",
+                columns: table => new
+                {
+                    PagoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    InscripcionId = table.Column<int>(nullable: false),
+                    Monto = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagos", x => x.PagoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Asignaturas");
+
+            migrationBuilder.DropTable(
+                name: "Estudiantes");
+
+            migrationBuilder.DropTable(
+                name: "Pagos");
+
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Inscripciones");
+
+            migrationBuilder.DropColumn(
+                name: "EstudianteId",
+                table: "Inscripciones");
+
+            migrationBuilder.DropColumn(
+                name: "Fecha",
+                table: "Inscripciones");
+
+            migrationBuilder.DropColumn(
+                name: "Monto",
+                table: "Inscripciones");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Semestre",
+                table: "Inscripciones",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
